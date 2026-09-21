74aa9c29c883042d40318cfdaf977753622d29c4
andulle12 <aliabdulle4616@gmail.com>
2026-09-21 20:09:25 +0300

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import LandingPage from './landingpage'
-import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   
 
   return (
     <>
-    <Analytics />
+    
 
      <LandingPage/>
     </>
